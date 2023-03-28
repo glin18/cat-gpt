aad5f4f9ff2a9bf179c6fc4c284eb2b10a9e9e0a
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-03-28 18:51:21 +0800

    Create table heading component

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { TableHeading } from "./TableHeading"
 
 export const MainContainer = () => {
   return (
     <div className="main-container">
         <div className="title">CatGPT</div>
+        <TableHeading/>
     </div>
   )
 }
---
+++ src/components/TableHeading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CatIcon from "../images/cat-icon.png"
+
+export const TableHeading = () => {
+  return (
+    <div className="table-heading">
+        <img src={CatIcon}></img>
+        <div>Examples</div>
+    </div>
+  )
+}
