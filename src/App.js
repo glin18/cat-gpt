46c5218ed1d939ff1914cd310e70b1ae156b6c2d
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-03-28 15:27:09 +0800

    Create main container and title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { LeftBar } from "./components/LeftBar"
+import { MainContainer } from "./components/MainContainer"
 
 function App() {
   return (
     <div className="App">
       <LeftBar/>
+      <MainContainer/>
     </div>
   );
 }
---
+++ src/components/MainContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MainContainer = () => {
+  return (
+    <div className="main-container">
+        <div className="title">CatGPT</div>
+    </div>
+  )
+}
