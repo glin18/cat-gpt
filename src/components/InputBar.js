a42ea9fa1ad0ea54714d6d67c783eeee80bdb2bf
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-03-29 11:20:32 +0800

    Add submit button to input

--- src/components/InputBar.js
+++ src/components/InputBar.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import SubmitArrow from "../images/submit-arrow.png"
 
 export const InputBar = () => {
   return (
     <div className="input-bar-container">
         <form>
             <input type="text"></input>
+            <button type="submit">
+                <img src={SubmitArrow}></img>
+            </button>
         </form>
     </div>
   )
