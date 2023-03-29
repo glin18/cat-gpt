bb5036d1ca6df23d029d8fb7ea37f0e4c09db4f8
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-03-29 14:17:52 +0800

    Create basic structure for first response

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { InputBar } from "./InputBar"
 import FaceIcon from "../images/smiling-emoticon-square-face.png"
+import CatIcon from "../images/cat-icon.png"
 
 export const ChatContainer = ({onSubmitHandler, onChangeHandler, question}) => {
   return (
@@ -12,7 +13,10 @@ export const ChatContainer = ({onSubmitHandler, onChangeHandler, question}) => {
             </div>
         </div>
         <div className="answer-container">
-
+            <div className="question-inner-container">
+                <img src={CatIcon}></img>
+                <div className="question">meow meow meow</div>
+            </div>
         </div>
         <InputBar onSubmitHandler={onSubmitHandler} onChangeHandler={onChangeHandler}/>
     </div>
