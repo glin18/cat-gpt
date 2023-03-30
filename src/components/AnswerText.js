6b89768f3f7ffcd7e3250678cd0d43cf838f9d16
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-03-30 22:00:37 +0800

    Add loading flicker while typing

--- src/components/AnswerText.js
+++ src/components/AnswerText.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from "react";
 
 export const AnswerText = ({ children }) => {
   const [revealedLetters, setRevealedLetters] = useState(children[0]);
+  const [isTyping, setIsTyping] = useState(true);
   const index = useRef(0);
 
   useEffect(() => {
@@ -12,9 +13,13 @@ export const AnswerText = ({ children }) => {
     if (index.current < children.length -1) {
       let addChar = setInterval(tick, 200);
       return () => clearInterval(addChar);
+    } else {
+      setIsTyping(false);
     }
   }, [revealedLetters]);
   return (
-    <div className="question">{revealedLetters}</div>
+    <>
+      <div className="question">{revealedLetters}{isTyping && <div className="loading"></div>}</div>
+    </>
   );
 };
